a1e9cc3b9670dc963d425df37c4ffb646e8db5e5
Dane Carmichael <carmichaeldane@gmail.com>
2016-09-19 08:08:22 -0500

    Adding the ability to add, subtract, multiply, and divide modifiers to rolls

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,10 +4,21 @@ class Message < ApplicationRecord
     if self.body[/\d{1,3}d\d{1,3}/]
       num_times = self.body[/\b\d{1,3}/].to_i
       num_of_sides = self.body[/\d{1,3}\b/].to_i
-      original = self.body[/\d{1,3}d\d{1,3}/]
-      rolls = num_times.times.collect { return_die_result(num_of_sides) }
-      self.body = "#{self.user_name} rolls #{original}, resulting in"\
-                  " *#{rolls.join(", ")}* for a total of *#{rolls.reduce(:+)}*"
+      original = self.body
+      attachment = self.body[/(- |\+\ |\/\ |\*\ )\d*/]
+      if attachment
+        opperator = attachment[0]
+        modifier = attachment[/\d{1,3}/]
+        original.gsub!(/(- |\+\ |\/\ |\*\ )\d*/, "#{opperator} #{modifier}")
+        rolls = num_times.times.collect { return_die_result(num_of_sides) }
+        self.body = "#{self.user_name} rolls #{original}, resulting in"\
+                    " *#{rolls.join(", ")}* for a total of"\
+                    "*#{rolls.reduce(:+).send(opperator.to_sym, modifier.to_i)}*"
+      else
+        rolls = num_times.times.collect { return_die_result(num_of_sides) }
+        self.body = "#{self.user_name} rolls #{original}, resulting in"\
+                    " *#{rolls.join(", ")}* for a total of *#{rolls.reduce(:+)}*"
+      end
     else
       rolls = 2.times.collect { return_die_result(6) }
       self.body = "#{self.user_name} rolls two six-sided die resulting in *#{rolls.join(", ")}*"\
@@ -19,3 +30,5 @@ class Message < ApplicationRecord
     SecureRandom.random_number(1..sides)
   end
 end
+
+# modifier and drop lowest or higher
