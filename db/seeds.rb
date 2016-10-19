11806d10bc63f77f1262beb04a9430faa6b91691
Matt Rice <mattrice12@outlook.com>
2016-10-19 17:38:00 -0500

    cleared the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-Actor.create!(name: "mattrice")
-Character.create!(name: "Thūm", actor_id: 1)
