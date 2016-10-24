00934d9ec0d15118f95be2c8784b2dd4263b4c52
Matt Rice <mattrice12@outlook.com>
2016-10-24 14:37:03 -0500

    added testing

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -9,6 +9,10 @@ class MessageTest < ActiveSupport::TestCase
     @msg3 = Message.new(body: "4d8 - 3", user_name: "dane")
     @msg4 = Message.new(body: "3d6 + 2 drop low", user_name: "dane")
     @msg5 = Message.new(body: "4d8 - 3 drop high", user_name: "dane")
+    @msg6 = Message.new(body: "str", user_name: "dane")
+    @msg7 = Message.new(body: "weapon", user_name: "dane")
+    @msg8 = Message.new(body: "2d6 str", user_name: "dane")
+    @msg9 = Message.new(body: "4d20 str drop high", user_name: "dane")
 
     @roll_params1 = { times_rolled: "2",
                       sides_to_die: "d6",
@@ -21,23 +25,36 @@ class MessageTest < ActiveSupport::TestCase
   end
 
   test "message parsing with no modifiers" do
-    assert_equal({ times_rolled: "3", sides_to_die: "d6", dropped_die: nil, attachment: nil },
+    assert_equal({ times_rolled: "3", sides_to_die: "d6", dropped_die: nil, attachment: nil, :stat_mod=>nil, :equipment_mod=>nil },
                  @msg1.parse_message)
   end
 
   test "message parsing with add and subtract modifiers" do
-    assert_equal({ times_rolled: "2", sides_to_die: "d6", dropped_die: nil, attachment: "+ 2" },
+    assert_equal({ times_rolled: "2", sides_to_die: "d6", dropped_die: nil, attachment: "+ 2", :stat_mod=>nil, :equipment_mod=>nil },
                  @msg2.parse_message)
-    assert_equal({ times_rolled: "4", sides_to_die: "d8", dropped_die: nil, attachment: "- 3" },
+    assert_equal({ times_rolled: "4", sides_to_die: "d8", dropped_die: nil, attachment: "- 3", :stat_mod=>nil, :equipment_mod=>nil },
                  @msg3.parse_message)
   end
 
   test "message parsing with add and subtract modifiers with drop high and low" do
-    assert_equal({ times_rolled: "3", sides_to_die: "d6", dropped_die: "drop", attachment: "+ 2" },
+    assert_equal({ times_rolled: "3", sides_to_die: "d6", dropped_die: "drop", attachment: "+ 2", :stat_mod=>nil, :equipment_mod=>nil },
                  @msg4.parse_message)
-    assert_equal({ times_rolled: "4", sides_to_die: "d8", dropped_die: "drop", attachment: "- 3" },
+    assert_equal({ times_rolled: "4", sides_to_die: "d8", dropped_die: "drop", attachment: "- 3", :stat_mod=>nil, :equipment_mod=>nil },
                  @msg5.parse_message)
   end
+
+  test "message parsing with stat modifiers" do
+    assert_equal({ times_rolled: nil, sides_to_die: nil, dropped_die: nil, attachment: nil, :stat_mod=>"str", :equipment_mod=>nil },
+                 @msg6.parse_message)
+    assert_equal({ times_rolled: nil, sides_to_die: nil, dropped_die: nil, attachment: nil, :stat_mod=>nil, :equipment_mod=>"weapon" },
+                 @msg7.parse_message)
+    assert_equal({ times_rolled: "2", sides_to_die: "d6", dropped_die: nil, attachment: nil, :stat_mod=>"str", :equipment_mod=>nil },
+                 @msg8.parse_message)
+    assert_equal({ times_rolled: "4", sides_to_die: "d20", dropped_die: "drop", attachment: nil, :stat_mod=>"str", :equipment_mod=>nil },
+                 @msg9.parse_message)
+  end
+
+
 
   test "numberize die" do
     assert_equal({ times_rolled: 2, sides_to_die: 6, modifier: nil, attachment: nil} ,
@@ -86,10 +103,12 @@ class MessageTest < ActiveSupport::TestCase
     result1 = @msg1.build_roll_message([2,6,4])
     result2 = @msg2.build_roll_message([4,5], Attachment.new("+", "2"))
     result3 = @msg4.build_roll_message([5,6], Attachment.new("+", "2"), 4)
+    result4 = @msg6.build_roll_message([5,6], nil, nil, [" +", 2])
 
     assert_equal("dane rolls 3d6, resulting in *2, 6, 4* for a total of *12*", result1)
     assert_equal("dane rolls 2d6 + 2, resulting in *4, 5* for a total of *11*", result2)
     assert_equal("dane rolls 3d6 + 2 drop low, resulting in *5, 6* for a total of *13* _dropped 4_",
                  result3)
+    assert_equal("dane rolls 2d6 str, resulting in *5, 6 +2* for a total of *13*", result4)
   end
 end
